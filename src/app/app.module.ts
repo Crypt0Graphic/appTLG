259316e63c7f53faf3d22d121a17622932932b21
Crypt0Graphic <fettahzafer@hotmail.com>
2018-09-04 14:16:49 +0300

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule, MatMenuModule, MatCardModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
