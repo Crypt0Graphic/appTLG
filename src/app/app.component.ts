5ce9a9eb272864fca8e29bb048d2429ee5079b5b
Crypt0Graphic <fettahzafer@hotmail.com>
2018-09-27 17:34:58 +0300

    modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  encapsulation: ViewEncapsulation.None,
+  styles: [
+    `
+      .dark-modal .modal-content {
+        background-color: #292b2c;
+        color: white;
+      }
+      .dark-modal .close {
+        color: white;
+      }
+      .light-blue-backdrop {
+        background-color: #5cb3fd;
+      }
+    `
+  ]
 })
 export class AppComponent {
+  constructor(private modalService: NgbModal) {}
+
   title = 'Tolga Dijital';
+
+  openModal(content, photoNumber) {
+    console.log(photoNumber);
+    this.modalService.open(content, { centered: true, size: 'lg' });
+  }
 }
